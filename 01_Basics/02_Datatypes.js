21e87051b484a02aa992093e304cc17c1a10ce26
gayatrisuvarnkar <gayatrisuvarnkar43@gmail.com>
2025-12-16 21:26:32 +0000

    Datatypes

---
+++ 01_Basics/02_Datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all JS code as newer version
+
+//alert(3 + 3) // we are using nodejs, not browser
+
+console.log(3
+     + 3) // code readability should be high
+
+console.log("Gayatri")
+
+let name = "Gayatri"
+let age = 18
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint
+// string =>""
+// bolean =>true/false
+// null   => standalone value
+// undefinied => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof "Gayatri"); //string
+console.log(typeof null);      //object
+console.log(typeof undefined); //undefined
